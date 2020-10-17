4891f1d1078e47798c399fc80eec35bb117de810
pranav1416 <pranav.borole@gmail.com>
2020-10-17 04:53:06 -0700

    Navigation

--- app.py
+++ app.py
@@ -2,32 +2,59 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import pyqtSlot, Qt
+from homescreen import homeScreen
 
 class Window(QMainWindow):
-    def __init__(self, *args, **kwargs):
-        super(QMainWindow, self).__init__(*args,**kwargs)
-        main_win = QWidget()
-        self.setWindowTitle("Elixir Math Quiz Game")
-
-        layout = QGridLayout()
-        
-        
-        label = QLabel("Main screen")
-        label.setAlignment(Qt.AlignCenter)
-        layout.addWidget(label)
-        
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self.btnAction)
-        layout.addWidget(btn)  
-        
-        main_win.setLayout(layout)
-        self.setCentralWidget(main_win)
-        self.show()
-    def btnAction():
-        print("Click works")
-        
     
-class homeScreen(self):
+    def __init__(self, *args, **kwargs):
+        super(Window, self).__init__(*args,**kwargs)
+        self.setWindowTitle("Elixir Math Quiz Game")
+        width = 1000
+        height = 1000
+        # self.setFixedHeight(height)
+        # self.setFixedWidth(width)
+        self.init_app()
+        # main_win = QWidget()
+        # self.setWindowTitle("Elixir Math Quiz Game")
+
+        # layout = QGridLayout()
+        
+        
+        # label = QLabel("Main screen")
+        # label.setAlignment(Qt.AlignCenter)
+        # layout.addWidget(label)
+        
+        # txt = QLineEdit()
+        # txt.setAlignment(Qt.AlignCenter)
+        # layout.addWidget(txt)
+
+        # btn = QPushButton("Click me!")
+        # btn.clicked.connect(self.btnAction)
+        # layout.addWidget(btn)  
+        
+        # main_win.setLayout(layout)
+        # self.setCentralWidget(main_win)
+        # self.show()
+    def init_app(self):
+        self.nav_panel = QListWidget()
+        #add navigation bar right side
+        self.nav_panel.insertItem(0, 'Home')
+        self.nav_panel.insertItem(1, 'Learn')
+
+        self.page_stack = QStackedWidget(self)
+        self.home = QWidget()
+        homeScreen(self.home)
+        #self.home.resize(self.home.sizeHint())
+        self.page_stack.addWidget(self.home)
+        
+        layout = QHBoxLayout(self)
+        #layout.addWidget(self.nav_panel)
+        layout.addWidget(self.page_stack)
+        self.setLayout(layout)
+        self.show()
+
+    
+
 
     
 def main():
