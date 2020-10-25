436b0aee315e22601d9951830e195a7396c53efd
Edward Liu <ed19911127@gmail.com>
2020-10-24 18:08:12 -0700

    fixed learning screen's buttons deleted learningWidge.py/.ui

--- screen/learning_screen.py
+++ screen/learning_screen.py
@@ -1,7 +1,9 @@
-from PyQt5 import QtWidgets,uic
+from PyQt5 import QtWidgets, uic
 from pathlib import Path
+from functools import partial
 
 ui_path = Path("./screen/_ui/")
+
 
 class LearningScreen(QtWidgets.QWidget):
 
@@ -9,3 +11,10 @@ class LearningScreen(QtWidgets.QWidget):
         super(LearningScreen, self).__init__(*args, **kwargs)
         #main_app_path = ui_path / "learning_screen.ui"
         uic.loadUi("./screen/_ui/learning_screen.ui", self)
+        self.addModule1Btn.clicked.connect(
+            partial(self.handleClickModuleBtn, 0))
+        self.addModule2Btn.clicked.connect(
+            partial(self.handleClickModuleBtn, 1))
+
+    def handleClickModuleBtn(self, index):
+        self.stackedWidget.setCurrentIndex(index)
