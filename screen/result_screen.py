218b10a4deaa96306cd9fdf5995ae16164f25019
Keira Chou <fangtingchou0308@gmail.com>
2020-10-27 14:56:05 +0800

    added arrow button function

--- screen/result_screen.py
+++ screen/result_screen.py
@@ -12,13 +12,30 @@ class ResultScreen(QtWidgets.QWidget):
         #main_app_path = ui_path / "result_screen.ui"
         uic.loadUi("./screen/_ui/result_screen.ui", self)
         self.Btn_menu_1.clicked.connect(
-            partial(self.handleClickModuleBtn, 0))
+            partial(self.handleClickMenuBtn, 0))
         self.Btn_menu_2.clicked.connect(
-            partial(self.handleClickModuleBtn, 1))
+            partial(self.handleClickMenuBtn, 1))
         self.Btn_menu_3.clicked.connect(
-            partial(self.handleClickModuleBtn, 2))
+            partial(self.handleClickMenuBtn, 2))
         self.Btn_menu_4.clicked.connect(
-            partial(self.handleClickModuleBtn, 3))
+            partial(self.handleClickMenuBtn, 3))
+        self.Btn_next_1.clicked.connect(
+            partial(self.handleClickPreNextBtn, 1))
+        self.Btn_next_2.clicked.connect(
+            partial(self.handleClickPreNextBtn, 2))
+        self.Btn_next_3.clicked.connect(
+            partial(self.handleClickPreNextBtn, 3))
+        self.Btn_pre_2.clicked.connect(
+            partial(self.handleClickPreNextBtn, 0))
+        self.Btn_pre_3.clicked.connect(
+            partial(self.handleClickPreNextBtn, 1))
+        self.Btn_pre_4.clicked.connect(
+            partial(self.handleClickPreNextBtn, 2))
+     
 
-    def handleClickModuleBtn(self, index):
+    def handleClickMenuBtn(self, index):
         self.stackedWidget.setCurrentIndex(index)
+     
+
+    def handleClickPreNextBtn(self, index):
+        self.tabWidget.setCurrentIndex(index)
